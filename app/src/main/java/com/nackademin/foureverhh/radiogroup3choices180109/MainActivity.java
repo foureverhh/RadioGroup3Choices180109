33907a9dff98f4da52da6dae865b2b1e74aa66b4
Yuanheng Li <foureverhh@yuanhengs-air.guest.stockholm.se>
2018-01-09 15:04:07 +0100

    Initial commit

---
+++ app/src/main/java/com/nackademin/foureverhh/radiogroup3choices180109/MainActivity.java
@@ -0,0 +1,42 @@
+package com.nackademin.foureverhh.radiogroup3choices180109;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View v)
+    {
+        TextView txv =(TextView)findViewById(R.id.txv);
+        RadioGroup ticketType =(RadioGroup)findViewById(R.id.ticketType);
+
+        /*
+        switch (ticketType.getCheckedRadioButtonId())
+        {
+            case R.id.adult:
+                txv.setText("Full ticket.");
+                break;
+            case R.id.child:
+                txv.setText("Child ticket");
+                break;
+            case R.id.elder:
+                txv.setText("Elder ticket");
+                break;
+        }
+        */
+        int id=ticketType.getCheckedRadioButtonId();
+        RadioButton select =(RadioButton)findViewById(id);
+        txv.setText("Buy "+select.getText());
+    }
+}
